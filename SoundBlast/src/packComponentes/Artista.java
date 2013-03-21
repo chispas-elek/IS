7c63ec953fbd53f4da37f5818aa20e176b095ef5
samvrai <t3echelon@gmail.com>
2013-03-21 09:52:27 +0100

    Inicio implementación

--- SoundBlast/src/packComponentes/Artista.java
+++ SoundBlast/src/packComponentes/Artista.java
@@ -1,5 +1,22 @@
+//Author -> Mulero Martínez, Rubén
+//Author -> Esteban García, Asier
+
 package packComponentes;
 
 public class Artista {
+	
+	//Variables
+	private String codArtista, nombre, tipo;
+	
+	//Methods
+	
+			//Constructor	
+	public Artista(String pCodArtista, String pNombre, String pTipo) {
+		this.codArtista = pCodArtista;
+		this.nombre = pNombre;
+		this.tipo = pTipo;
+	}
+	
+	
 
 }
